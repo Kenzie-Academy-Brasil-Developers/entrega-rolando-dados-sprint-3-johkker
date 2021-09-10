a763364aade393433e5c142efcbe7a5fd608345c
Johnny Gregório <johkker@gmail.com>
2021-09-10 15:08:24 -0300

    Funçao de armazenar os 1000 giros criada. Botao para ativar a funçao criado.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Para começar, faça o clone desse repositório.
 Ao rolar dois dados de seis faces, cada dado irá mostrar um número entre 1 e 6. Quando estes números nos dois dados forem somados, o resultado será um número entre 2 e 12. **Multiplicar o resultado por 2 não é o mesmo que rolar dois dados**
 
 Use JavaScript para simular 1000 lançamentos de um par de dados. *Você precisará descobrir como gerar números aleatórios no JavaScript, use suas habilidades de busca no Google!*
-
+0
 Para cada possível lançamento, conte a frequência. Você deve usar um array para registrar essas contagens.
 
 Inicialize um array chamado *`count`* preenchendo-o com zeros. Em seguida, sempre que uma rolagem de dados resultar em, digamos, sete, adicione um ao valor do 7º elemento do array de contagem.
--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+const dice1 = [1,2,3,4,5,6]
+const dice2 = [1,2,3,4,5,6]
+let count = [0,0,0,0,0,0,0,0,0,0,0]
 
+function rollDice() {
+    for(let i = 0; i <= 1000; i++) {
+    let result1 = dice1[Math.floor(Math.random() * dice1.length)]
+    let result2 = dice2[Math.floor(Math.random() * dice2.length)]
+    let sum = result1 + result2
+    if(sum === 2){
+        count[0] += 1
+    } 
+    if(sum === 3){
+        count[1] += 1
+    }
+    if(sum === 4){
+        count[2] += 1
+    }
+    if(sum === 5){
+        count[3] += 1
+    }
+    if(sum === 6){
+        count[4] += 1
+    }
+    if(sum === 7){
+        count[5] += 1
+    }
+    if(sum === 8){
+        count[6] += 1
+    }
+    if(sum === 9){
+        count[7] += 1
+    }
+    if(sum === 10){
+        count[8] += 1
+    }
+    if(sum === 11){
+        count[9] += 1
+    }
+    if(sum === 12){
+        count[10] += 1
+    }
+    
+  }
+  
+  return count
+}
+
+// as duas linhas abaixo fazem minha função rollDice ser ativada somente quando clicar no botão
+const rollDiceButton = document.getElementById("rollDiceID");
+rollDiceButton.addEventListener('click', rollDice);
+
+
+const resultSpace = document.getElementById("resultSpace");
+const result = document.createElement("div");
